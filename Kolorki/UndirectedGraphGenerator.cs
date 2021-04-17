d7af5a0e499957a4f16103c391f4dee632ab12b1
xarix <dmduda17@gmail.com>
2021-04-17 13:00:45 +0200

    Lab #1

--- Kolorki/UndirectedGraphGenerator.cs
+++ Kolorki/UndirectedGraphGenerator.cs
@@ -7,11 +7,11 @@ namespace Kolorki
         public UndirectedGraph GenerateRandomInstance()
         { 
             var random = new Random();
-            var graph = new UndirectedGraph(random.Next(1, 30));
+            var graph = new UndirectedGraph(100);
             var numberOfVertices = graph.GetNumberOfVertices();
             var maxEdges = numberOfVertices * (numberOfVertices - 1) / 2;
 
-            for (int i = 0; i < random.Next(1, maxEdges); i++)
+            for (int i = 0; i < 5000; i++)
             {
                 graph.AddEdge(random.Next(1, numberOfVertices), random.Next(1, numberOfVertices));
             }
